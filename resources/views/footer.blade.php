6abd845cbca364d18c77ab0544eadad6457486b7
elraffa <federico@wcanvas.com>
2022-11-16 16:38:04 -0300

    4-feature: changed footer structure

--- resources/views/footer.blade.php
+++ resources/views/footer.blade.php
@@ -2,32 +2,50 @@
  <footer class="footer">
      <div class="footer__content container">
 
-         <div class="row">
+         <div class="footer__row">
 
-             <div class="footer__col-1">
-                 <div class="title__heading-4 bold">Hola</div>
+             <div class="footer__col--40">
+                 <div class="title__heading-4 bold">
+                     <img src="{{ asset('/img') }}/logo.png" width="150" />
+                 </div>
+             </div>
+             <div class="footer__col">
+                 <h4 class="title__heading-4 bold">Términos Legales</h4>
+                 <h4 class="title__heading-4 bold">Datos Personales</h4>
+                 <h4 class="title__heading-4 bold">Correo Electrónico</h4>
+                 <h4 class="title__heading-4 bold">Home page</h4>
+
+             </div>
+             <div class="footer__col">
+                 <div class="title__heading-4 bold">Regístrate</div>
+                 <div class="title__heading-4 bold">Escritorio</div>
+
              </div>
 
-             <div class="footer__col-1">
-                 <div class="title__heading-4 bold">Hola</div>
+             <div class="footer__col">
+                 <div class="title__heading-4 bold">
+                    Síguenos en
+                 </div>
+                 <div class="footer__social">
+                    <a href="https://www.facebook.com/ompersonalenglish"><i class="fa fa-facebook"></i></a>
+                    <a href="https://twitter.com/ompersonal"><i class="fa fa-twitter"></i></a>
+                    <a href="#"><i class="fa fa-linkedin"></i></a>
+                    <a href="#"><i class="fa fa-instagram"></i></a>
+                    <a href="#"><i class="fa fa-youtube"></i></a>
+                 </div>
              </div>
 
-             <div class="footer__col-1">
-                 <div class="title__heading-4 bold">Hola</div>
-             </div>
+
 
          </div>
-         <div class="row">
 
-             <div class="footer__col-1">
-                 <div class="footer__copyright">
-                     <p class="footer__text-legales"><span class="bold">2022 - OM PERSONAL MULTIMEDIA ENGLISH: Desde 1999 en
-                             Internet.<br>© Orlando Moure - Todos los derechos reservados</span>
-                     <p class="footer__text-legales"> Buenos Aires, República Argentina | <a href="http://www.ompersonal.com.ar">Dashboard page</a> | Correo: info@ompersonal.com.ar</p>
-                     <p class="footer__text-legales"> Términos Legales | Política de Tratamiento de Datos Personales</p>
-                     <p class="footer__text-legales">Queda absolutamente prohibida la reproducción o descarga de contenidos de
-                         este portal.</p>
-                 </div>
+         <div class="footer__row">
+
+             <div class="footer__copyright">
+                 <p class="footer__text-legales"><span class="bold"><?php echo date('Y'); ?> - OM PERSONAL MULTIMEDIA ENGLISH: Desde 1999 en Internet.<br>© Orlando Moure - Todos los derechos reservados</span>
+                 <p class="footer__text-legales"> Buenos Aires, República Argentina </p>
+                 <p class="footer__text-legales">Queda absolutamente prohibida la reproducción o descarga de contenidos de
+                     este portal.</p>
              </div>
          </div>
 
@@ -36,7 +54,6 @@
  </footer>
 
  </div>
- <!-- <script src="https://cdn.jsdelivr.net/npm/swiper@8/swiper-bundle.min.js"></script> -->
  </body>
 
  </html>
