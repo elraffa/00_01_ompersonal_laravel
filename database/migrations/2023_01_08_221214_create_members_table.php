2c6375fdfac4c58578566bdda9c0b1ecd701c690
Humberto Domizi <luchodomizi@gmail.com>
2023-01-09 13:35:26 -0300

    migrations for members

---
+++ database/migrations/2023_01_08_221214_create_members_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('members', function (Blueprint $table) {
+            $table->id();
+            $table->string('name', 100);
+            $table->string('last_name', 100);
+            $table->string('email', 100);
+            $table->unsignedBigInteger('country_id')
+                ->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('members');
+    }
+};
