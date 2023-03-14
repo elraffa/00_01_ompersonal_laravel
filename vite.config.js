62dd1962ad275f5fd13ce27dfc9c60fa84978424
elraffa <federico@wcanvas.com>
2023-03-14 14:06:44 -0300

    change vite config file

--- vite.config.js
+++ vite.config.js
@@ -5,8 +5,11 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
     plugins: [
         laravel({
-            input: 'resources/js/app.jsx',
-            refresh: true,
+            input: [ 'resources/js/app.jsx',
+            'resources/sass/main.scss',
+            'resources/js/app.js',
+        ],
+            refresh: true, 
         }),
         react(),
     ],
