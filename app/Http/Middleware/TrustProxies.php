72b9656cb2fec59ecebc9bf3018b85a3893b0597
elraffa <federico@wcanvas.com>
2023-06-19 13:22:50 -0300

    Revert "add trusted proxies"
    This reverts commit fb6f9906f54c60a8eed0118fe4d89004ff4f2e4d.

--- app/Http/Middleware/TrustProxies.php
+++ app/Http/Middleware/TrustProxies.php
@@ -19,7 +19,6 @@ class TrustProxies extends Middleware
      *
      * @var int
      */
-    protected $proxies = '*';
     protected $headers =
         Request::HEADER_X_FORWARDED_FOR |
         Request::HEADER_X_FORWARDED_HOST |
