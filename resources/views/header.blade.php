a3f40a04fb4474cc676fc626087d97da9692cde0
elraffa <federicoruffa@gmail.com>
2023-08-14 18:00:37 -0300

    add favicons

--- resources/views/header.blade.php
+++ resources/views/header.blade.php
@@ -8,6 +8,11 @@
 
         <!-- Fonts -->
         <link href="https://fonts.bunny.net/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
+        <!--Favicons-->
+        <link rel="apple-touch-icon" sizes="180x180" href="{{ asset('/img/favicons/apple-touch-icon.png') }}">
+        <link rel="icon" type="image/png" sizes="32x32" href="{{ asset('/img/favicons/favicon-32x32.png') }}">
+        <link rel="icon" type="image/png" sizes="16x16" href="{{ asset('/img/favicons/favicon-16x16.png') }}">
+        <link rel="manifest" href="{{ asset('/img/favicons/site.webmanifest') }}">
 
         <!-- Styles -->
         <link rel="stylesheet" href="/css/main.css">
