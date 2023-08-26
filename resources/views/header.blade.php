8c6653d8cb6ab98e0c61c72115600a587a2e0186
elraffa <federicoruffa@gmail.com>
2023-08-26 09:29:32 -0300

    add google verification code

--- resources/views/header.blade.php
+++ resources/views/header.blade.php
@@ -3,6 +3,7 @@
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta name="google-site-verification" content="r5zXrN_DF4mQiAvoFgRGBSC-lr_iQRkNEXFAvBGmKps" />
 
         <title>OMPersonal English</title>
 
